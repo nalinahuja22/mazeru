52f5c8453025fb06a608855abcb5868f2a680a4c
Nalin Ahuja <nalinahuja22@gmail.com>
2021-01-23 14:24:22 -0800

    Project Backup

--- bin/timeline.py
+++ bin/timeline.py
@@ -1,7 +1,15 @@
 # Developed by matthew-notaro, nalinahuja22, and ClarkChan1
 
-class Timeline:
-    def __init__(self):
-        pass
+class Cut:
+    def __init__(self, vfile, itime, otime):
+        self.vfile = vfile
+        self.itime = itime
+        self.otime = otime
 
-        asdf
+class Timeline:
+    def __init__(self, adata, vdata):
+        self.adata = adata
+        self.vdata = vdata
+
+    def render(self):
+        pass
