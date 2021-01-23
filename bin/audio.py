fcae31c099d0337819d783de7d4f817685713ee9
Nalin Ahuja <nalinahuja22@gmail.com>
2021-01-23 18:13:31 -0500

    Project Backup

--- bin/audio.py
+++ bin/audio.py
@@ -3,6 +3,10 @@
 import os
 import sys
 import librosa
+import matplotlib.pyplot as plt
+
+def avg(l):
+    return (sum(l) / len(l))
 
 class Audio:
     def __init__(self, audio_file):
@@ -13,10 +17,19 @@ class Audio:
         self.track = []
 
     def analyze(self):
-        data, sr = librosa.load(self.audio_file, mono = False)
+        duration = int(librosa.get_duration(filename = self.audio_file))
 
-        print(data)
+        l = []
+
+        for i in range(duration):
+            data, sr = librosa.load(self.audio_file, offset = i, duration = 1, mono = False)
+            rms = (librosa.feature.rms(data))[0]
+
+            l.append(avg(rms))
+
+        plt.plot(l)
+        plt.show()
 
 
-obj = Audio("../media/audio/wav/reminisce.wav")
+obj = Audio("../media/audio/reminisce.wav")
 obj.analyze()
