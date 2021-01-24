95b9257d6cdf2206b9c620a24f4af3cddcdf6833
Clark Chan <chan239@purdue.edu>
2021-01-24 00:29:12 -0500

    fixed bug with optional arguments

--- bin/main.py
+++ bin/main.py
@@ -11,11 +11,12 @@ def main():
     parser.add_argument("audio_path", help="path to audio file")
     parser.add_argument("video_path", help="path to directory that contains videos")
     parser.add_argument("output_path", help="path to where the output file will be stored")
-    parser.add_argument("--min_seconds", help="minimum threshold time interval to cut", type=int)
-    parser.add_argument("--max_seconds", help="maximum threshold time interval to cut", type=int)
+    parser.add_argument("min_seconds", help="minimum threshold time interval to cut", type=int, nargs='?')
+    parser.add_argument("max_seconds", help="maximum threshold time interval to cut", type=int, nargs='?')
     args = parser.parse_args()
 
-    vid_maker = Timeline(args.audio, args.video)
+
+    vid_maker = Timeline(args.audio_path, args.video_path)
 
     if args.min_seconds:
         vid_maker.min_thr = args.min_seconds
